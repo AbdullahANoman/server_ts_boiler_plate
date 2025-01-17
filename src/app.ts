2f502f3df3c212a17e29d9cf731eded6ae732690
Abdullah Al Noman <abdullahnoman4537@gmail.com>
2025-01-17 23:48:40 +0600

    added eslint to find problem in compiler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
 
-app.get("/", (req, res) => {
+//NOTE parser
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export default app;
---
+++ src/app/config/index.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import path from "path";
+
+//NOTE this path.join((process.cwd())) works like you add the env with this directory
+dotenv.config({ path: path.join((process.cwd(), ".env")) });
+
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+};
